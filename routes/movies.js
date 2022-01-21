c9914bb977588f224090adaee44456c375065a81
Kurolius <ymajbar.pro@gmail.com.com>
2022-01-21 12:27:56 +0100

    Final in class

--- routes/movies.js
+++ routes/movies.js
@@ -1,12 +1,23 @@
-var express = require('express');
-var router = express.Router();
-const fs = require('fs');
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    
-    let rawdata = fs.readFileSync('./public/json/movies_reduit.json');
-    let movies = JSON.parse(rawdata);
-    res.send(movies);
-});
+const express = require('express');
+const router = express.Router();
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+ 
 
+router.get('/', async function (req, res, next) {
+    const take = parseInt(req.query.take) || 10;
+    const skip = parseInt(req.query.skip) || 0;
+    const movies = await prisma.movies.findMany({
+        skip: skip,
+        take: take
+    })
+    res.send({
+        data: movies,
+        pagination: {
+            count: movies.length, // Total des enregistrements
+            take: take,   // Nombre d'éléments sélectionnés
+            skip: skip   // Décalage à partir duquel on prend les  données
+        }
+    });
+});
 module.exports = router;
